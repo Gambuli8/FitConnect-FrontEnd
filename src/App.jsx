d9a8e0940cfe5d5954d4f09daa0708e2520e2c32
Manuel Rodriguez Sabella <manursabella@gmail.com>
2023-07-06 18:05:26 -0300

    NavBar PROTOTYPE, hice estilos, en mainjsx envolvi al App en BrowserRouter. En App.jsx hice algunas rutas para los componentes creados actualmente, hice que la navbar se vea siempre

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Actividades from "./views/Actividades/Actividades";
+import Home from "./views/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
+import Membresias from "./views/Membresias/Membresia";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/actividades" element={<Actividades />} />
+        <Route path="/membresias" element={<Membresias />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../images/fitconnectimg.png";
 const NavBar = () => {
   return (
-    <div>NavBar</div>
-  )
-}
+    <nav class="p-1 bg-black shadow md:flex md:items-center md:justify-between">
+      <div>
+        <span>
+          <img class="h-20 inline" src={logo}></img>
+        </span>
+      </div>
 
-export default NavBar
+      <ul class="md:flex md:items-center">
+        <li class="mx-4">
+          <Link
+            to="/"
+            class="text-white text-xl hover:text-yellow-500 duration-500 "
+          >
+            Inicio
+          </Link>
+        </li>
+        <li class="mx-4">
+          <Link
+            to={"/contacto"}
+            class="text-white text-xl hover:text-yellow-500 duration-500"
+          >
+            Contacto
+          </Link>
+        </li>
+        <li class="mx-4">
+          <Link
+            to={"/membresias"}
+            class="text-white text-xl hover:text-yellow-500 duration-500"
+          >
+            Membresias
+          </Link>
+        </li>
+        <li class="mx-4">
+          <Link
+            to={"/actividades"}
+            class="text-white text-xl hover:text-yellow-500 duration-500"
+          >
+            Actividades
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;

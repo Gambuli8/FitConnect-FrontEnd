a4602cd3896af65927ce0763eb9c1ff928aaadd3
GeroGambuli <gerogambuli2002@gmail.com>
2023-07-08 17:32:00 -0300

    finalizado section activities y semiterminado el footer

--- src/components/Card_Actividades/Card_Actividades.jsx
+++ src/components/Card_Actividades/Card_Actividades.jsx
@@ -1,6 +1,6 @@
 function Card_Actividades() {
   return (
-    <div className="w-[340px] h-[450px] border-[4px] border-[#ffd277] shadow-black shadow-xl bg-gray-800  flex flex-col rounded-xl justify-center items-center m-4 bg-auto bg-no-repeat bg-center bg-origin-padding hover:translate-y-[-20px] duration-300">
+    <div className="w-[340px] h-[450px] border-[4px] border-[#ffd277] shadow-black shadow-xl bg-black  flex flex-col rounded-xl justify-center items-center m-4 bg-auto bg-no-repeat bg-center bg-origin-padding hover:translate-y-[-20px] duration-300">
       <div className="flex flex-col text-justify p-2 items-center text-white">
         <img
           src="https://thumbs.dreamstime.com/b/meditaci%C3%B3n-de-yoga-y-mujer-tranquila-en-la-alfombra-para-el-ejercicio-respiratorio-bienestar-cuerpo-saludable-estudio-espiritual-277878330.jpg"
@@ -11,7 +11,7 @@ function Card_Actividades() {
           nombre de actividad
         </h1>
         <h3 className="font-parrafo my-2 text-[20px]">horario de actividad</h3>
-        <p className="font-Inconsolata my-2 text-[17px] font-light capitalize text-gray-400">
+        <p className="font-Inconsolata my-2 text-[17px] font-medium capitalize text-gray-400">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore
           blanditiis minus ut necessitatibus quos sed laborum, dolore
           voluptatibus deleniti? Minus mollitia impedit ipsa voluptate

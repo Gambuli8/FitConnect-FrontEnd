4ca2d43570226a7c3241e8f1f312d893ee8ba824
Manuel Rodriguez Sabella <manursabella@gmail.com>
2023-07-10 02:17:23 -0300

    fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-  return (
-    <div>Footer 23</div>
-  )
-}
+  return <div>Footer 23</div>;
+};
 
-export default Footer
+export default Footer;

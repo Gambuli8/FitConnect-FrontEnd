bef6160a78314055ce1db56235de4bb20c501f04
Manuel Rodriguez Sabella <manursabella@gmail.com>
2023-07-10 12:52:03 -0300

    fixed footer y reutilizado en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Actividades from "./views/Actividades/Actividades";
 import Home from "./views/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
 import Membresias from "./views/Membresias/Membresia";
+import Footer from "./components/Footer/Footer";
 import "./App.css";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/actividades" element={<Actividades />} />
         <Route path="/membresias" element={<Membresias />} />
       </Routes>
+      <Footer />
     </div>
   );
 }

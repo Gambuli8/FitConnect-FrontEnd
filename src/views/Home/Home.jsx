4f67ae177bf6ac41d643302572fa06218f20ac2a
Sacha Figueredo <sacha_figueredo@hotmail.com>
2023-07-10 13:24:03 -0300

    primer commit

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,7 +1,8 @@
-function Home() {
+
+
+export default function Home() {
   return (
     <div>Home</div>
+
   )
 }
-
-export default Home
--- src/views/Membresias/Membresia.jsx
+++ src/views/Membresias/Membresia.jsx
@@ -1,6 +1,13 @@
 function Membresia() {
   return (
-    <div>Membresia</div>
+    <div>
+      <h2>Membresia</h2>
+      <p>Price: $10/month</p>
+      <p>Duration: 1 year</p>
+      <p>Features: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <button>Buy Now</button>
+    </div>
+
   )
 }
 

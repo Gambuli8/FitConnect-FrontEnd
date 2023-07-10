a1b56f3d4c3e2a462cd46a8635567b9d94eef558
Manuel Rodriguez Sabella <manursabella@gmail.com>
2023-07-10 02:26:03 -0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
- 
-import './App.css'
-import Actividades from './views/Actividades/Actividades'
-
-function App() {
-
-
 import { Route, Routes } from "react-router-dom";
 import Actividades from "./views/Actividades/Actividades";
 import Home from "./views/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
 import Membresias from "./views/Membresias/Membresia";
 import "./App.css";
- 
 
 function App() {
   return (
- 
-    <div className='App'> 
-      <Actividades/>
-    </div>
-  )
-
     <div>
       <NavBar />
       <Routes>
@@ -30,8 +16,6 @@ function App() {
       </Routes>
     </div>
   );
-
- 
 }
 
 export default App;
